ec1e944c9b1fbcd6f196d2e32f20375ec7b99bed
Jenesis-22 <jacksonabraham@gmail.com>
2026-07-17 16:39:12 +1000

    Add files via upload

---
+++ Value.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal)
+#user input
+name = input("enter your name: ")
+print("\n hello", name, "\n welcome to codingal")
