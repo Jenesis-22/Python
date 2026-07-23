25faa35d1f3424b6f9a36b025ac831791aa1f626
Jenesis-22 <jacksonabraham@gmail.com>
2026-07-23 11:59:58 +1000

    Add files via upload

---
+++ Schooltimes.py
@@ -0,0 +1,8 @@
+# waking up
+print("i wake up at 7 am")
+# going to school
+print("i go to school at 8 am")
+# playing after school
+print("i play with my friends in the evening")
+# studying
+print("i study for 2 hours at night.")
