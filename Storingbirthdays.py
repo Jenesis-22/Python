de18911bda48cecbed5277da6285f3526101f65e
Jenesis-22 <jacksonabraham@gmail.com>
2026-07-24 12:23:38 +1000

    Add files via upload

---
+++ Storingbirthdays.py
@@ -0,0 +1,8 @@
+# My birthday
+print("my birthday is 2nd december")
+# Brother's birthday
+print("my brother's birthday is 2nd september")
+# Mum's birthday
+print("my mom's birthday is 10th september")
+# Dad's birthday
+print("my dad's birthday is 25th november")
