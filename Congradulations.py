6a54aef6dbb7d79b0519cf0f2fce5d7ebaf189f4
Jenesis-22 <jacksonabraham@gmail.com>
2026-07-24 12:35:28 +1000

    Add files via upload

---
+++ Congradulations.py
@@ -0,0 +1,10 @@
+# Being excited
+print("wow you won a gold medal at state?")
+# friend
+print("yes it was a close finish but i managed to take the lead")
+# raj
+print("You must be a really good athlete")
+# friend
+print(" thank you")
+#raj
+print("i have to leave but congradulations for winning at state")
