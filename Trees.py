14c1f81be493dd54686e90cee75ae03400d4d1ec
Jenesis-22 <jacksonabraham@gmail.com>
2026-07-24 14:06:48 +1000

    Add files via upload

---
+++ Trees.py
@@ -0,0 +1,12 @@
+# storing values
+tree1 = 96
+tree2 = 64
+tree3 = 39
+tree4 = 72
+tree5 = 101
+# finding the total of tree's
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is : ", sum)
+# finding the average of all the trees
+average = sum/5
+print("the average of all the trees is: ", average)
